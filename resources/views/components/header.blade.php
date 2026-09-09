53c4fca28d0629ec070191fe7b1977b0af98cf98
DJAWANNI <ivanivashenko203@gmail.com>
2026-09-09 15:18:38 +0500

    думается мега еще

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<header class="m-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2>шапка лютая</h2>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<x-head/>
+<body>
+    <x-header/>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12 text-center mb-5">
+                <h2>
+                    Кнопки мега
+                </h2>
+            </div>
+            <div class="col-3"><button class="btn btn-primary">1</button></div>
+            <div class="col-3"><button class="btn btn-primary">2</button></div>
+            <div class="col-3"><button class="btn btn-primary">3</button></div>
+        </div>
+    </div>
+    <x-scripts/>
+</body>
+
+</html>
