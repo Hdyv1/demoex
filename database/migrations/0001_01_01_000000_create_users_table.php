908a6afb60e4ed3ae832ea91c8e868acb1c8e186
DJAWANNI <ivanivashenko203@gmail.com>
2026-09-09 14:59:44 +0500

    миграции, полу реквесты, модели

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
+            $table->string('email');
+            $table->string('fullname');
+            $table->string('phonenumber');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
 
